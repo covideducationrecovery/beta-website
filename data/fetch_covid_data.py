30bfcaa123d624c29283fb02792b380606fac2c8
Camden Shultz <45770625+CWrecker@users.noreply.github.com>
2021-06-15 12:10:45 -0400

    Add files via upload

--- data/fetch_covid_data.py
+++ data/fetch_covid_data.py
@@ -14,22 +14,22 @@ def update_json_file():
     
     
     url_data = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/latest/owid-covid-latest.csv'
-    url_look_up = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/UID_ISO_FIPS_LookUp_Table.csv'
+    url_look_up = 'countries_codes.csv'
     
     
     data_all = pd.read_csv(url_data, error_bad_lines=False).loc[: , ['iso_code','total_cases','total_deaths','total_vaccinations','people_vaccinated','people_fully_vaccinated']]
-    data_look_up = pd.read_csv(url_look_up, error_bad_lines=False).loc[: , ['iso2','iso3']].groupby(['iso3']).head(1)
+    data_look_up = pd.read_csv(url_look_up, error_bad_lines=False, keep_default_na=False, na_values=[""]).loc[: , ['iso2','iso3']].groupby(['iso3']).head(1)
     
     result = data_all.to_json(orient ='records')
     parsed = json.loads(result)
     
     # Add iso2 Code
-    result_iso = data_look_up.set_index('iso3').T.to_dict(orient = 'dict')
+    result_iso = dict(zip(data_look_up.iso3, data_look_up.iso2))
     
     for item in parsed:
         iso2_code = result_iso.get(item['iso_code'])
         if iso2_code:
-            item['iso2'] = iso2_code['iso2']
+            item['iso2'] = iso2_code
         else:
             parsed.remove(item)
             print("Item Removed:", item)
