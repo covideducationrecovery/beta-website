4748685230acdd65a875b33ccbc6214b676d3d63
Camden Shultz <45770625+CWrecker@users.noreply.github.com>
2021-07-22 09:44:49 -0400

    Update generateStaticFiles.sh

--- data/scripts/generateStaticFiles.sh
+++ data/scripts/generateStaticFiles.sh
@@ -1,6 +1,6 @@
 # get the needed data from airtable to process
-node -r esm -r dotenv/config airtable-downloader.js
+node -r dotenv/config airtable-downloader.js
 
-node -r esm -r dotenv/config generate-static-countries-files-for-public-website.js
-node -r esm -r dotenv/config get-worldbank-data.js
-node -r esm -r dotenv/config inject-indicators-into-countries.js
+node generate-static-countries-files-for-public-website.js
+node get-worldbank-data.js
+node inject-indicators-into-countries.js
