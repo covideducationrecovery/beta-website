dd78ef04f32175bd6d5caf898a3671459b14a5db
Camden Shultz <45770625+CWrecker@users.noreply.github.com>
2021-07-22 09:46:38 -0400

    Delete generateStaticFiles.sh

--- data/scripts/generateStaticFiles.sh
+++
@@ -1,6 +0,0 @@
-# get the needed data from airtable to process
-node -r dotenv/config airtable-downloader.js
-
-node generate-static-countries-files-for-public-website.js
-node get-worldbank-data.js
-node inject-indicators-into-countries.js
